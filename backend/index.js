bebde57bcfb1942d727fed7567b3bcfb2ba94aff
Muhammad Sadiq <muhammadsadiqrajani@gmail.com>
2024-08-25 18:32:13 +0500

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import { app } from "./src/app.js";
+import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
+
+dotenv.config({
+    path: './.env' 
+ })
+
+connectDB()
+.then(() => {
+    //check for errors before starting app
+    app.on('error' , (error) => {
+        console.log("Error starting the app : ", error);  
+    })
+    //Starting app
+    app.listen((process.env.PORT || 3000) , () => {
+        console.log("App running on port" , process.env.PORT);
+        
+    })
+})
+.catch((error) => {
+    console.log("In app.js , Error connecting DB : ",error);
+})
+
+
