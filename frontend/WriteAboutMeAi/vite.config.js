33210d04075a9461f2ff64123fb6116253adee41
Muhammad Sadiq <muhammadsadiqrajani@gmail.com>
2024-08-25 18:16:31 +0500

    Update vite.config.js

--- frontend/WriteAboutMeAi/vite.config.js
+++ frontend/WriteAboutMeAi/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy : {
-      '/api' : 'https://write-about-me-ai.vercel.app/'
+      '/api' : 'https://write-about-me-ai.vercel.app'
     }
   } , 
   plugins: [react()],
