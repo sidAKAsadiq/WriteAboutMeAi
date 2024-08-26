9ca6dbee50b1e209e00ed6f539bcdfa013279ab3
Muhammad Sadiq <muhammadsadiqrajani@gmail.com>
2024-08-26 12:41:28 +0500

    Update user.routes.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,7 +3,13 @@ import { generate_about_section, get_current_user, login_user, logout_user, regi
 import { verify_jwt } from "../middlewares/auth.middleware.js"
 
 const router = Router()
-
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
 router.route('/register').post(register_user)
 router.route('/login').post(login_user)
 router.route('/logout').get(verify_jwt,logout_user)
