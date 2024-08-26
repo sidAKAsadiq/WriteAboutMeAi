93225db9546d8d6cff69ebd27b08a362efdae393
Muhammad Sadiq <muhammadsadiqrajani@gmail.com>
2024-08-26 18:14:47 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN, //for now, allowing from all sources
     credentials: true,
+    methods : ["GET" , "POST"],
 }))
 app.use(express.json({limit : '16kb'})) //accept json data (forms)
 app.use(express.urlencoded({extended : true , limit : '16kb'})) //Accept from URLs
